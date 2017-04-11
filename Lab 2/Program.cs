c4cb0c01e2ec7c67e74e9100654a68b938994f87
Jonas <jkpaskus@gmail.com>
2017-04-11 14:38:43 -0400

    Committing first attempt with odd/even

---
+++ Lab 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////input
+            //DateTime input;
+            //Console.Write("Enter a number between 1 and 100: ");
+            //input = DateTime.Parse(Console.ReadLine());
+
+            //if (input.Year < 1000)
+            //{
+            //    Console.WriteLine("This date is in the first Mill.");
+            //}
+            //else if (input.Year < 2000)
+            //{
+            //    Console.WriteLine("This date is in the second Mill.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This date is in the third Mill.");
+            //}
+
+            //-------------------------------------------------------------------------------------
+
+            int i;
+            Console.Write("Enter a number between 1 and 100: \n");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is {0} and even", i);
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is {0} and odd", i);
+                Console.Read();
+            }
+        }
+    }
+}
